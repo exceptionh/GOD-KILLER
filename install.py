3d9139e8b9b3614b13877dcbb3c9c5a8626a8b2f
exceptionh <114394523+exceptionh@users.noreply.github.com>
2022-11-19 10:23:31 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import os
 run = os.system
 print("eshek")
-if print =='eshek' or choice=='eshek1':
+if print =='eshek' or print=='eshek1':
 
     run('chmod 777 GOD-KILLER')
     run('mkdir /usr/share/god-killer')
@@ -12,7 +12,7 @@ if print =='eshek' or choice=='eshek1':
         file.write(cmnd)
     run('chmod +x /usr/bin/GOD-KILLER & chmod +x /usr/share/god-killer/GOD-KILLER')
     print('''\n\ncongratulation GOD-KILLER is installed successfully \nfrom now just type \x1b[6;30;42mGOD-KILLER\x1b[0m in terminal ''')
-if print=='eshek' or choice=='eshek1':
+if print=='eshek' or print=='eshek1':
     run('rm -r /usr/share/god-killer ')
     run('rm /usr/bin/GOD-KILLER ')
     print('[!] now GOD-KILLER has been removed successfully')
